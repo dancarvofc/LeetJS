6d66f203f9008e78e5f8518a45c4ed42ccc15aab
Danilo Carvalho <contato.danilolc@gmail.com>
2025-03-09 21:45:20 -0300

    Implement Add Two Numbers solution in JavaScript

---
+++ 2.add-two-numbers.js
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=2 lang=javascript
+ *
+ * [2] Add Two Numbers
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+    let carry = 0;
+    let dummy = new ListNode(0);
+    let current = dummy;
+    while (l1 || l2) {
+        let sum = carry;
+        if (l1) {
+            sum += l1.val;
+            l1 = l1.next;
+        }
+        if (l2) {
+            sum += l2.val;
+            l2 = l2.next;
+        }
+        carry = Math.floor(sum / 10);
+        current.next = new ListNode(sum % 10);
+        current = current.next;
+    }
+    if (carry > 0) {
+        current.next = new ListNode(carry);
+    }
+    return dummy.next;
+};
+// @lc code=end
